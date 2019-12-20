4c28572bdc1993c3a8a9a6e03a087f55f61dece9
vsvipul <vsvipul555@gmail.com>
2019-12-20 15:40:55 +0530

    Add code for location tracking

--- App.js
+++ App.js
@@ -1,12 +1,68 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default class HelloWorldApp extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
-      </View>
-    );
-  }
+export default class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            ready: false,
+            where: {lat:null, lng:null},
+            error: null
+        }
+    }
+
+    componentDidMount(){
+        let geoOptions = {
+            enableHighAccuracy: true,
+            timeOut: 20000,
+            maximumAge: 3000,
+            distanceFilter: 0
+        };
+        this.setState({ready:false, error: null });
+        navigator.geolocation.watchPosition(this.geoSuccess, this.geoFailure, geoOptions);  
+    }
+
+    geoSuccess = (position) => {
+        console.log(position.coords);
+        
+        this.setState({
+            ready:true,
+            where: {lat: position.coords.latitude,lng:position.coords.longitude }
+        });
+    }
+    
+    geoFailure = (err) => {
+      alert('Please turn on GPS');
+      this.setState({error: err.message});
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                { !this.state.ready && (
+                <Text style={styles.big}>Using Geolocation in React Native.</Text>
+                )}
+                { this.state.error && (
+                <Text style={styles.big}>{this.state.error}</Text>
+                )}
+                { this.state.ready && (
+                    <Text style={styles.big}>{
+                    `Latitude: ${this.state.where.lat}
+                    Longitude: ${this.state.where.lng}`
+                    }</Text>
+                )}
+            </View>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    big: {
+        fontSize: 48
+    }
+});
