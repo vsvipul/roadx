6e2b6c02e4f6675256a2fbde6e69b5307c191bcd
vsvipul <vsvipul555@gmail.com>
2019-12-21 12:35:34 +0530

    add camera button

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Button, TouchableOpacity } from 'react-native';
 import { Accelerometer } from 'expo-sensors';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
-import Notif from './notif';
+import MapView from 'react-native-maps';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Camera } from 'expo-camera';
+
 
 export default class App extends React.Component {
     constructor(){
@@ -76,23 +79,88 @@ export default class App extends React.Component {
         });
     }
     
+    geoFailure = (err) => {
+      alert('Please turn on GPS');
+      this.setState({error: err.message});
+    }
+
+    async snapPhoto() {
+      console.log('Camera Button Pressed');
+      
+    }
+
     render() {
         return (
+            // <View style={styles.container}>
+            //     { !this.state.ready && (
+            //     <Text style={styles.big}>Using Geolocation in React Native.</Text>
+            //     )}
+            //     { this.state.error && (
+            //     <Text style={styles.big}>{this.state.error}</Text>
+            //     )}
+            //     { this.state.ready && (
+            //         <Text style={styles.big}>{
+            //         `Latitude: ${this.state.where.lat}
+            //         Longitude: ${this.state.where.lng}`
+            //         }</Text>
+            //     )}
+            // </View>
             <View style={styles.container}>
-                <Notif />
+              <MapView style={styles.mapStyle} />
+              <View
+                  style={{
+                      position: 'absolute',//use absolute position to show button on top of the map
+                      top: '50%', //for center align
+                      alignSelf: 'flex-end' //for align to right
+                  }}
+              >
+                  {/* <Button style={styles.btnstyle} title="+" /> */}
+                  <TouchableOpacity 
+                    style={{ borderWidth:1,
+                      borderColor:'rgba(0,0,0,0.2)',
+                      alignItems:'center',
+                      justifyContent:'center',
+                      width:70,
+                      height:70,
+                      backgroundColor:'#fff',
+                      borderRadius:100,
+                      marginTop: 270,
+                      marginRight: 30
+                    }}
+                    onPress={this.snapPhoto.bind(this)}
+                    >
+                    <Icon name={"camera"}
+                      size={30}
+                      color="#01a699" />
+                  </TouchableOpacity>
+              </View>
             </View>
         );
     }
 }
 
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center'
+//     },
+//     big: {
+//         fontSize: 48
+//     }
+// });
+
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    big: {
-        fontSize: 48
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });
